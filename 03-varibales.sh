86746a14d7a51ce78ff388bb82e5588fd8ce1baf
pavanraj45 <pavanraju13@gmail.com>
2025-01-02 10:46:56 +0530

    arguments passed

---
+++ 03-varibales.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRIEND1=$1
+FRIEND2=$2
+echo " $FRIEND2 : Hi $FRIEND1, How are you?"
+echo " $FRIEND1 : Hi $FRIEND2. I am fine. What about you?"
+echo " $FRIEND2 : I am fine.Are you looking for upgrade your skills?'
+echo "$FRIEND1 : yes $FRIEND2 . Do you know any platform to learn devops?"
+echo "$FRIEND2 : yes. Siva devops is the best.
