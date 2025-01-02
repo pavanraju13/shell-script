0ae85678403fb5bfe192d01bf3434c2eeff3cb43
pavanraj45 <pavanraju13@gmail.com>
2025-01-02 10:06:36 +0530

    names changed

--- variables.sh
+++ variables.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 
-FRIEND1=Ramesh
-FRIEND2=Suresh
+FRIEND1=sachin
+FRIEND2=rahul
 echo " $PERSON2 : Hi $PERSON1, How are you?"
 echo " $PERSON1 : Hi $PERSON2. I am fine. What about you?"
 echo " $PERSON2 : I am fine."
