b24d0410f4d0477c5ee46e5ba28bbe272064392d
pavanraj45 <pavanraju13@gmail.com>
2025-01-08 10:28:32 +0530

    changes

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter the username :"
+
+read  username
+
+echo "Please enter the password :"
+
+read password
+
+echo " Username is : $username" "Password is : $password"
