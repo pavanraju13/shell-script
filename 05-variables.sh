fa0c4fd66c1511fad11ec2b256493d4830b5ad1d
pavanraj45 <pavanraju13@gmail.com>
2025-01-08 10:59:09 +0530

    movies list

---
+++ 05-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+Movies = ("Indra" "Wailter veeraya" "Khaidi")
+
+#size of the array is 3
+
+#Index are 0 1 2
+
+#starts with 0
+
+
+
+echo "first movie is : ${movies[0 ]}"
+
+echo "Second movie is : ${movies[1]}"
+
+echo "List of movies is : ${movies[@]}"
+
+read movies
