3dd0ed2985a2b32ab0293e9d1656e6f7e58d2584
pavanraj45 <pavanraju13@gmail.com>
2025-01-08 10:32:47 +0530

    changes made

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "Please enter the username :"
+# Prompt for username
+echo  "Please enter the username: "
+read username
 
-read  username
+# Prompt for password (hidden input)
+echo  "Please enter the password: "
+read -s password
+echo # Print a blank line for formatting
 
-echo "Please enter the password :"
-
-read password
-
-echo " Username is : $username" "Password is : $password"
+# Display the input
+echo "Username is: $username"
+echo "Password is: $password"
