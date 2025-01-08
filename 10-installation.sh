ae93db48addbd1e21516237260c138c565736a04
pavanraju13 <pavanraju4513@gmail.com>
2025-01-08 15:23:35 +0530

    changes

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,4 +1,17 @@
+
+
+
+
+
 #!/bin/bash
 
-
-dnf install mysql -y
+# Check if the script is run as root
+if [ $(id -u) -eq 0 ]; then
+  # Run the command if the user is root
+  echo "Installing MySQL..."
+  dnf install mysql -y
+else
+  # Throw an error if not run as root
+  echo "Error: This script must be run as root!"
+  exit 1
+fi
