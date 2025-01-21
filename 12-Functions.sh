13ed8e512990eebc75aada6939a5350f05ff402e
pavanraju13 <pavanraju4513@gmail.com>
2025-01-21 07:26:15 +0530

    changes

--- 12-Functions.sh
+++ 12-Functions.sh
@@ -3,12 +3,23 @@
 
 ID=$(id -u )
 
-if [ $ID -eq 0 ]
+
 
 VALIDATE() {
     echo "exit status : $1"
     echo " what are you doing : $2"
 }
+if [ $ID -eq 0 ]
+
+then
+
+echo " you are super user"
+
+else
+
+echo "Required sudo access"
+
+fi
 
 dnf install mysql -y
 VALIDATE $? "Install mysql"
@@ -16,4 +27,3 @@ dnf install git -y
 VALIDATE $?  "Install git"
 
 
-fi
