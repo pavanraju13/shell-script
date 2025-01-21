a7b7b29d700fc78f68c3bc2ff5b47df51aa5f0ec
pavanraju13 <pavanraju4513@gmail.com>
2025-01-21 14:41:51 +0530

    changes

---
+++ 16.Installationpacakges.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ID=$(id -u)
+G="\e[31m"
+R="\e[32m"
+N="\e[0m"
+TIMESTAMP=$( date +%F-%H-%M-%S )
+SCRIPT_NAME=$(basename $0 | cut "." -f1 )
+LOGFILE=/tmp/${SCRIPT_NAME}-${TIMESTAMP}.log
+
+if [ $ID -eq 0]
+then
+  echo "you are root user"
+
+else
+ echo "you need root access"
+
+fi
+
+echo "all packages : $@"
