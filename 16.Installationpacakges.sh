5a7ae5d147517eceabaa62ce53382dca43075d52
pavanraju13 <pavanraju4513@gmail.com>
2025-01-21 14:48:50 +0530

    changes

--- 16.Installationpacakges.sh
+++ 16.Installationpacakges.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 ID=$(id -u)
-G="\e[31m"
-R="\e[32m"
-N="\e[0m"
-TIMESTAMP=$( date +%F-%H-%M-%S )
-SCRIPT_NAME=$(basename $0 | cut "." -f1 )
-LOGFILE=/tmp/${SCRIPT_NAME}-${TIMESTAMP}.log
+
 
 if [ $ID -eq 0]
 then
