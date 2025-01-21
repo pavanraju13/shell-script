e4256820a0ac40e51becfe92a1041e058593c0f7
pavanraju13 <pavanraju4513@gmail.com>
2025-01-21 08:21:16 +0530

    changes

---
+++ 13-logs.sh
@@ -0,0 +1,30 @@
+#1/bin/bash
+
+ID=$(id -u)
+TIME_STAMP=$date+%F-%H-%M-%S
+SCRIPT_NAME=$(echo "$0 | cut -d "." -f1")
+log_file=\tmp\$SCRIPT_NAME-$TIME_STAMP.LOG
+
+VALIDATE(){
+
+
+if [ $1 -eq 0 ]
+
+then 
+     echo "$2 ..success"
+    
+else
+     echo "$2....failure"
+
+     exit 1
+
+fi
+}
+
+
+     dnf install mysql -y &>>$log_file
+     VALIDATE $? "install mysql"
+
+     dnf install git -y &>>$log_file
+
+     VALIDATE $? "install git"
