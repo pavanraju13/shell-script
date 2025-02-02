db93afe98663b8a2d7ce4764357ec14d7fcfc77d
pavanraju13 <pavanraju4513@gmail.com>
2025-02-02 19:31:08 +0530

    this is calling other script

--- 18-shellscript.sh
+++ 18-shellscript.sh
@@ -6,6 +6,3 @@ echo "display the before content : $Devops"
 echo " display the arguments passed : $@"
 echo "display the script name : $0"
 echo "display the count of the variables : $#"
-./17-otherscript.sh
-
-echo "display the after content : $Devops"
