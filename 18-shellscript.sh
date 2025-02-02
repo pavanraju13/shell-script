6617e68f36bde58b307838b8afc54ce45baa198e
pavanraju13 <pavanraju4513@gmail.com>
2025-02-02 17:35:47 +0530

    this is calling other script

--- 18-shellscript.sh
+++ 18-shellscript.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-Content="Contentaftercode"
+Devops="course after the code"
 
-echo "display the before content : $Content"
+echo "display the before content : $Devops"
 echo " display the variables count : $#"
+echo "display the script name : $0"
+echo " display the passed varibales :$@"
 ./17-otherscript.sh
 
 echo "display the after content : $Content"
