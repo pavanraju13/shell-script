9a1be2effebb0c3215809d9e8996e5445bdac510
pavanraju13 <pavanraju4513@gmail.com>
2025-02-03 13:42:02 +0530

    make the changes

--- 18-shellscript.sh
+++ 18-shellscript.sh
@@ -6,7 +6,7 @@ echo "Before calling other script, course: $COURSE"
 
 echo "process ID of the current shell script : $$"
 
-Source ./17-otherscript.sh
+source ./17-otherscript.sh
 
 echo "After calling the other script , course: $COURSE"
 
