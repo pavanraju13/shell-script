72bd7017689878c0ae6bea551af437ea3bc07672
pavanraju13 <pavanraju4513@gmail.com>
2025-02-03 12:47:16 +0530

    this is calling other script

--- 18-shellscript.sh
+++ 18-shellscript.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-Devops="course after the code"
+COURSE="Devops from current script"
 
-echo "display the before content : $Devops"
-echo " display the arguments passed : $@"
+echo "Before calling other script: $COURSE"
+echo " display the arguments passed : $#"
+echo "process ID of the current shell script : $$"
+
+./17-othersript.sh
+
 echo "display the script name : $0"
-echo "display the count of the variables : $#"
+echo "display the count of the variables : $@"
+echo "process ID of the other script : $$"
+
