e18ea0646cc927058ccbfa41ed266b5a778cd418
pavanraju13 <pavanraju4513@gmail.com>
2025-02-05 16:13:22 +0530

    make the changes

--- 10-installation.sh
+++ 10-installation.sh
@@ -9,12 +9,16 @@
 
 USERID=$(id -u)
 
+set -e
+
 if [ $USERID -eq 0 ]; then
   # Run the command if the user is root
   echo "Installing MySQL..."
-  dnf install mysql -y
+  
 else
   # Throw an error if not run as root
   echo "Error: This script must be run as root!"
   exit 1
 fi
+
+dnf install mysql -y
