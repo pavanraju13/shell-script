9b338bb23849be963d50d2c3a20849b102f1d49a
pavanraju13 <pavanraju4513@gmail.com>
2025-02-05 17:29:38 +0530

    make the changes

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,7 +10,7 @@
 USERID=$(id -u)
 
 failure (){
-  echo -e "${/e[31m .."Error occured at line no : $1 , error command :$2 ${/e0m"
+  echo -e "${/e[31m .."Error occured at line no : $1 , error command :$2 ${/e[0m"
 }
 
 trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
