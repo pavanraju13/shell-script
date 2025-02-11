b38e02e191edc61b5e0a8c0725a0054f130b6edd
pavanraju13 <pavanraju4513@gmail.com>
2025-02-11 08:58:01 +0530

    deleting old files

---
+++ deleteoldfiles.sh
@@ -0,0 +1,17 @@
+#/!bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+# Define colors
+G="\e[32m" # Green for success
+R="\e[31m" # Red for failure
+B="\e[34m" # Blue for informational
+N="\e[0m"  # Reset to default
+
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+echo "$G source directory already exists $N"
+else
+echo "$R Please make sure to create diectory $N"
+fi
